07b81f38b8d8cf0f79ad9a06f64db0f2ca4992a2
miguelmcolon <mm@miguelmcolon.com>
2021-11-30 12:06:34 +0100

    Adds Exercise 1-1
    Presumably the very first "hello, world" example in programming history.

---
+++ K&R-C/Chapter-1/Ex0101.c
@@ -0,0 +1,15 @@
+/* 
+   Imprime las palabras
+   
+   hello, world
+
+   Ejercicio 1-1. Ejecuta este programa en tu sistema. Experimenta y elimina algunas 
+   partes para ver qué mensajes de error se producen.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("hello, world\n");
+}
