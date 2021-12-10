5cec3bcc37a8e3b49b3e3b25188ce5cab5b55942
miguelmcolon <mm@miguelmcolon.com>
2021-12-10 20:03:10 +0100

    Adds Ejercicio 1-3

---
+++ El Entorno de Programación Unix - EEPU/1 - UNIX para principiantes/Ex0103.sh
@@ -0,0 +1,7 @@
+# Ejercicio 1-3. Intente
+#
+# ls /usr/games
+
+ls /usr/games
+
+# Es probable que no exista un directorio "games" en la ubicación referida si utiliza determinados equipos. Por ejemplo, no existe en las versiones recientes de Mac OS. No está probado en distribuciones de Linux donde, quizá, sí estén instalados en ese directorio del sistema. 
